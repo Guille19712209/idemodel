c899c5f4d5238e47cb36569fdcb3f02e65aef7fd
Guillermo Silvestrini <guillesilvestrini@gmail.com>
2026-04-16 14:09:37 -0300

    ada

--- docs/js/api.js
+++ docs/js/api.js
@@ -14,7 +14,6 @@ const supabaseClient = createClient(
   "sb_publishable_tNeS3BfRScwEchCnj6H_-w_YiZF_49N"
 );
 
-// opcional para debug
 window.supabaseClient = supabaseClient;
 
 window.addEventListener("DOMContentLoaded", init);
@@ -23,7 +22,8 @@ async function init() {
 
   console.log("INIT...");
 
-  const { data: { user } } = await supabaseClient.auth.getUser();
+  const { data } = await supabaseClient.auth.getUser();
+  const user = data.user;
 
   console.log("USER:", user);
 
